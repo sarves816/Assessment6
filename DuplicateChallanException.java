ad8044eb7fcf779d8219be800dd3fd36289248de
sarves816 <sarvesamrita@gmail.com>
2026-09-10 15:09:40 +0530

    Create DuplicateChallanException.java

---
+++ DuplicateChallanException.java
@@ -0,0 +1,11 @@
+package com.ast.challan;
+
+public class DuplicateChallanException
+        extends Exception {
+
+    public DuplicateChallanException(
+            String message) {
+
+        super(message);
+    }
+}
